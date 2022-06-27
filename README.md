807bd05d877323f748297382b5bebf858745cf35
purdyjen <purdyjen@me.com>
2022-06-27 15:48:20 -0400

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # bookmark-app
+
+- Define the Problem
+    - Need for a bookmarking app to save websites
+    - initially use local storage, so account not needed
+    - Add/Display/Removing Bookmarks
+- Confirm inputs
+    - (Required) Name/URL (required, user input)
+        - MUST be provided (because we're not a browser) - cannot be null/empty
+    - (Optional) Note/Description - to elaborate, help remind users what the site is
+- Test cases:
+    - should fail if Name/URL input empty (required attribute)
+- Brainstorming (Think about and choose solution)
+    - User form with two required inputs: Name and URL (MVP)
+        - optional note input
+    - Structured as object containing objects
+- Runtime Analysis
+    - Big-O Notation - Time/Space Complexity of solution
+    - Becomes more of an issue with a greater number of items, but since this is dependent upon the user to add new items, shouldn't be a significant factor
+- Coding
+    - const Bookmarks = {}
+    - add new Bookmark
+        - bookmark.name
+        - bookmark.url
+        - bookmark.dateAdded
+        - bookmark.note (default value of null unless user-defined)
+- Testing/Debugging
+
+
+
+
+Implementation/Expansion options:
+- site title as default "placeholder" text - (more advanced coding, because ability to fetch the url and grab the title is needed)
+- Folder/Organizing System
+    - default save location (or option for last used if a bookmark was added less than an hour ago?)
+    - User-Defined Categories ("folders")
+- Sorting Options 
+    - organize alphabetically by name
+    - organize by date added
+- Check if URL is a duplicate
+- Use Class/Subclass functionality
---
+++ script.js
@@ -0,0 +1,146 @@
+const modal = document.getElementById("modal");
+const modalShow = document.getElementById("show-modal");
+const modalClose = document.getElementById("close-modal");
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarksContainer = document.getElementById("bookmarks-container");
+
+let bookmarks = [];
+
+// Show Modal, Focus on Input
+function showModal() {
+  modal.classList.add("show-modal");
+  websiteNameEl.focus();
+}
+
+// Modal event listeners
+modalShow.addEventListener("click", showModal);
+modalClose.addEventListener("click", () =>
+  modal.classList.remove("show-modal")
+);
+window.addEventListener("click", (e) =>
+  e.target === modal ? modal.classList.remove("show-modal") : false
+);
+
+// Validate form
+function validate(nameValue, urlValue) {
+  const expression =
+    /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert("Please submit values for both fields.");
+    return false;
+  }
+  if (!urlValue.match(regex)) {
+    alert("Please provide a valid web address.");
+    return false;
+  }
+  // Valid
+  return true;
+}
+
+// Build Bookmarks
+function buildBookmarks() {
+  // Remove all bookmark elements
+  bookmarksContainer.textContent = "";
+  // Build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // Item
+    const item = document.createElement("div");
+    item.classList.add("item");
+    // Close icon
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fas", "fa-times");
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
+    // Favicon / Link Container
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+
+    // Favicon
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute("alt", "Favicon");
+    // Link
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
+// Fetch bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage if available
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    // Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: "google",
+        url: "google.com",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
+
+// Delete bookmark
+function deleteBookmark(url) {
+  // Loop through the bookmarks array
+  bookmarks.forEach((bookmark, i) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(i, 1);
+    }
+  });
+  // Update bookmarks array in localStorage, re-populate DOM
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
+}
+
+
+
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    // Add 'https://' if not there
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+      urlValue = `https://${urlValue}`;
+    }
+    // Validate
+    if (!validate(nameValue, urlValue)) {
+      return false;
+    }
+    // Set bookmark object, add to array
+    const bookmark = {
+      name: nameValue,
+      url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    // Set bookmarks in localStorage, fetch, reset input fields
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
+  }
+  
+  // Event Listener
+  bookmarkForm.addEventListener('submit', storeBookmark);
+  
+  // On Load, Fetch Bookmarks
+  fetchBookmarks();
+
+  //   The little '(e)' thing is actually part of broader scope of something in Javascript called an event handling function. Every event handling function receives an event object. For the purpose of this discussion, think of an object as a "thing" that holds a bunch of properties (variables) and methods (functions), much like objects in other languages. The handle, the 'e' inside the little (e) thing, is like a variable that allows you to interact with the object (and I use the term variable VERY loosely).
